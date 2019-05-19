c10d8add386a4cffaa6c7830eaffc178cfd60d85
FT <faisal.ajmal@gmail.com>
2019-05-19 22:26:00 +0300

    integrated Pika's consumer example

--- shared/amqp.py
+++ shared/amqp.py
@@ -25,11 +25,11 @@ class ThreadedAmqp(Thread):
     messages that have been sent and if they've been confirmed by RabbitMQ.
 
     """
-    EXCHANGE = 'incoming'
+    EXCHANGE = 'ftp'
     EXCHANGE_TYPE = 'direct'
-    PUBLISH_INTERVAL = 1
-    QUEUE = 'incoming'
-    ROUTING_KEY = 'incoming.queued'
+    #PUBLISH_INTERVAL = 1
+    QUEUE = 'from.ftp'
+    ROUTING_KEY = 'new.request'
 
     def __init__(self, group=None, target=None, name=None,
                  args=(), kwargs=None):
@@ -41,6 +41,9 @@ class ThreadedAmqp(Thread):
         self._connection = None
         self._channel = None
 
+        self._closing = False
+        self._consumer_tag = None
+
         self._deliveries = None
         self._acked = None
         self._nacked = None
@@ -49,16 +52,30 @@ class ThreadedAmqp(Thread):
         self._stopping = False
         self._url = None
 
+        self._consumerMode = False
+        self._consuming = False
+        self._prefetch_count = 1
+        self.should_reconnect = False
+        self.was_consuming = False
 
         return
 
-    def init(self, amqp_url):
+    def init(self, amqp_url, 
+        exchange = None, exchangeType = None, queue = None, routingKey = None, 
+        consumerMode = False):
         """Setup the example publisher object, passing in the URL we will use
         to connect to RabbitMQ.
 
         :param str amqp_url: The URL for connecting to RabbitMQ
+        :param bool consumerMode: If true publishing is disabled, if false consuming is disabled
 
         """
+
+        self.EXCHANGE = exchange
+        self.EXCHANGE_TYPE = exchangeType
+        self.QUEUE = queue
+        self.ROUTING_KEY = routingKey
+        self._consumerMode = consumerMode
 
         self._url = amqp_url
 
@@ -231,9 +248,130 @@ class ThreadedAmqp(Thread):
         response from RabbitMQ. Since we know we're now setup and bound, it's
         time to start publishing."""
         LOGGER.info('Queue bound')
-        self.start_publishing()
 
-    def start_publishing(self):
+        if self._consumerMode is False:
+            self.prep_publishing()
+            LOGGER.info("consumer mode is false, working in publishing mode.")
+        else:
+            LOGGER.info("consumer mode is false, working in consumer mode.")
+            self.set_qos()
+            pass
+
+    def set_qos(self):
+        """This method sets up the consumer prefetch to only be delivered
+        one message at a time. The consumer must acknowledge this message
+        before RabbitMQ will deliver another one. You should experiment
+        with different prefetch values to achieve desired performance.
+
+        """
+        self._channel.basic_qos(
+            prefetch_count=self._prefetch_count, callback=self.on_basic_qos_ok)
+
+    def on_basic_qos_ok(self, _unused_frame):
+        """Invoked by pika when the Basic.QoS method has completed. At this
+        point we will start consuming messages by calling start_consuming
+        which will invoke the needed RPC commands to start the process.
+
+        :param pika.frame.Method _unused_frame: The Basic.QosOk response frame
+
+        """
+        LOGGER.info('QOS set to: %d', self._prefetch_count)
+        self.start_consuming()
+
+    def start_consuming(self):
+        """This method sets up the consumer by first calling
+        add_on_cancel_callback so that the object is notified if RabbitMQ
+        cancels the consumer. It then issues the Basic.Consume RPC command
+        which returns the consumer tag that is used to uniquely identify the
+        consumer with RabbitMQ. We keep the value to use it when we want to
+        cancel consuming. The on_message method is passed in as a callback pika
+        will invoke when a message is fully received.
+
+        """
+        LOGGER.info('Issuing consumer related RPC commands')
+        self.add_on_cancel_callback()
+        self._consumer_tag = self._channel.basic_consume(
+            self.QUEUE, self.on_message)
+        self.was_consuming = True
+        self._consuming = True
+
+    def add_on_cancel_callback(self):
+        """Add a callback that will be invoked if RabbitMQ cancels the consumer
+        for some reason. If RabbitMQ does cancel the consumer,
+        on_consumer_cancelled will be invoked by pika.
+
+        """
+        LOGGER.info('Adding consumer cancellation callback')
+        self._channel.add_on_cancel_callback(self.on_consumer_cancelled)
+
+    def on_consumer_cancelled(self, method_frame):
+        """Invoked by pika when RabbitMQ sends a Basic.Cancel for a consumer
+        receiving messages.
+
+        :param pika.frame.Method method_frame: The Basic.Cancel frame
+
+        """
+        LOGGER.info('Consumer was cancelled remotely, shutting down: %r',
+                    method_frame)
+        if self._channel:
+            self._channel.close()
+
+    def on_message(self, _unused_channel, basic_deliver, properties, body):
+        """Invoked by pika when a message is delivered from RabbitMQ. The
+        channel is passed for your convenience. The basic_deliver object that
+        is passed in carries the exchange, routing key, delivery tag and
+        a redelivered flag for the message. The properties passed in is an
+        instance of BasicProperties with the message properties and the body
+        is the message that was sent.
+
+        :param pika.channel.Channel _unused_channel: The channel object
+        :param pika.Spec.Basic.Deliver: basic_deliver method
+        :param pika.Spec.BasicProperties: properties
+        :param bytes body: The message body
+
+        """
+        LOGGER.info('Received message # %s from %s: %s',
+                    basic_deliver.delivery_tag, properties.app_id, body)
+        self.acknowledge_message(basic_deliver.delivery_tag)
+
+    def acknowledge_message(self, delivery_tag):
+        """Acknowledge the message delivery from RabbitMQ by sending a
+        Basic.Ack RPC method for the delivery tag.
+
+        :param int delivery_tag: The delivery tag from the Basic.Deliver frame
+
+        """
+        LOGGER.info('Acknowledging message %s', delivery_tag)
+        self._channel.basic_ack(delivery_tag)
+
+    def stop_consuming(self):
+        """Tell RabbitMQ that you would like to stop consuming by sending the
+        Basic.Cancel RPC command.
+
+        """
+        if self._channel:
+            LOGGER.info('Sending a Basic.Cancel RPC command to RabbitMQ')
+            cb = functools.partial(
+                self.on_cancelok, userdata=self._consumer_tag)
+            self._channel.basic_cancel(self._consumer_tag, cb)
+
+    def on_cancelok(self, _unused_frame, userdata):
+        """This method is invoked by pika when RabbitMQ acknowledges the
+        cancellation of a consumer. At this point we will close the channel.
+        This will invoke the on_channel_closed method once the channel has been
+        closed, which will in-turn close the connection.
+
+        :param pika.frame.Method _unused_frame: The Basic.CancelOk frame
+        :param str|unicode userdata: Extra user data (consumer tag)
+
+        """
+        self._consuming = False
+        LOGGER.info(
+            'RabbitMQ acknowledged the cancellation of the consumer: %s',
+            userdata)
+        self.close_channel()
+
+    def prep_publishing(self):
         """This method will enable delivery confirmations and schedule the
         first message to be sent to RabbitMQ
 
@@ -305,6 +443,10 @@ class ThreadedAmqp(Thread):
 
         """
         if self._channel is None or not self._channel.is_open:
+            return
+        
+        if self._consumerMode is True:
+            logger.error("Trying to publish message when consumerMode is true, illegal operation.")
             return
 
         properties = pika.BasicProperties(
